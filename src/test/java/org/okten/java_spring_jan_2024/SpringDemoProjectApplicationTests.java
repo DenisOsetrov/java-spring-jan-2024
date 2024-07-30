afb4a7e84bfeb069ac0d64a62d8ae70957445361
dvose <dvosetrov@gmail.com>
2024-07-30 15:57:03 +0300

    Initial commit

---
+++ src/test/java/org/okten/java_spring_jan_2024/SpringDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okten.java_spring_jan_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
